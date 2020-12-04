cfcc70164f3c7236001ce60c5cf05cea0f725327
Brandon Berger <bdon@brandons-mbp.home>
2020-12-04 13:05:34 -0500

    Updates

--- public/js/rng.js
+++ public/js/rng.js
@@ -1,21 +1,115 @@
+let blackList = [
+    11941, // looting bag
+    7872, // firegiant bone
+    532, // big bones
+    526, // bones
+    7812, // goblin skull
+    592, // ashes
+    7875, // ice giant ribs
+    12934, // zulrah scales
+    22124, // sup dragon bones
+    1751, // blue d'hide
+    23490, // Larrans key
+    20543, // elite reward casket
+    12073, // elite clue scroll 
+    11940, // dark fishing bait
+    19679, // dark totem base
+    19683, // dark totem top
+    19677, // ancient shard
+    21257, // slayer's enchantment
+    19681, // dark totem middle
+    314, // feathers
+    23083, // brimstone key
+    1747, // black dragon hide
+    536, // dragon bones
+    19812, // key elite
+    11942, // Ecumenical_key
+    7884, // troll bone
+    7869, // moss giant bone
+];
+
+function getDropTable(monster) {
+    $.ajax({
+        method: "GET",
+        url: 'https://api.osrsbox.com/monsters?where={"name": "'+monster+'"}'
+    }).done(function(res) {
+        handleItemDrops(res._items[0].drops, monster);
+    });
+}
+
+function handleItemDrops(dropTable, monster) {
+    var item = roll(dropTable);
+    addToLootBag(monster, item);
+    paintLootBag(loot);
+    // console.log(loot);
+}
+
+
+function findMin(table) {
+    var min = 1;
+    for (var i = 0; i < table.length; i++) {
+        if (table[i].rarity < min) {
+            min = table[i].rarity;
+        } else {
+            continue;
+        }
+    }
+    return min;
+}
+
+function findMax(table) {
+    var max = 0;
+    for (var i = 0; i < table.length; i++) {
+        if (table[i].rarity > max) {
+            max = table[i].rarity;
+        } else {
+            continue;
+        }
+    }
+
+    return max;
+}
+
+
 function getSumWeight(table) {
     var weightSum = 0;
     for (var i = 0; i < table.length; i++) {
-        weightSum += table[i].weight;
+        weightSum += table[i].rarity;
     }
     return weightSum;
 }
 
-function rng(weightSum) {
-    return Math.floor(Math.random() * (weightSum - 1));
+function rng(min, max, sum) {
+    // return (Math.random() * (max - min) + min).toFixed(3);
+    return (Math.random() * sum).toFixed(3);
+}
+
+function shuffle(table) {
+    for (var i = 0; i < table.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (table.length - i));
+
+        var temp = table[j];
+        table[j] = table[i];
+        table[i] = temp;
+    }
+    return table;
 }
 
 function roll(table) {
     itemFound = false;
 
-    while (!itemFound) {
-        weightSum = getSumWeight(table);
-        randomNumber = rng(weightSum);
+    table = shuffle(table);
+    // console.log(table);
+
+    var countRolls = 0;
+
+    while (!itemFound && countRolls < 100) {
+        countRolls++;
+        min = findMin(table);
+        max = findMax(table);
+        sum = getSumWeight(table);
+        randomNumber = rng(min, max, sum);
+        // console.log(min, max, randomNumber);
         item = searchItem(randomNumber, table);
 
         if (item) {
@@ -26,15 +120,17 @@ function roll(table) {
 }
 
 function searchItem(randomNumber, table) {
-    for (var i = 0; i < table.length; i++) {
-        if (randomNumber < table[i].weight) {
-            if (table[i].table) {
-                return roll(table[i].table)
-            } else {
-                return table[i].name;
+    search: for (var i = 0; i < table.length; i++) {
+        if (randomNumber < table[i].rarity) {
+
+            if (table[i].rarity == 1) continue search;
+
+            for (var b = 0; b < blackList.length; b++) {
+                if (table[i].id == blackList[b]) continue search;
             }
+            return table[i];
         } else {
-            randomNumber -= table[i].weight;
+            randomNumber -= table[i].rarity;
         }
     }
 }
@@ -52,7 +148,20 @@ function monsterInLootBag(monster) {
 function addToLootBag(monster, item) {
     if (monsterInLootBag(monster)) {
         let index = loot.findIndex(x => x.monster == monster);
-        loot[index].drops.push(item);
+        var duplicate = false;
+        for (var i = 0; i < loot[index].drops.length; i++) {
+            if (item.id == loot[index].drops[i].id) {
+                var newQuantity = (parseInt(loot[index].drops[i].quantity) + parseInt(item.quantity)).toString();
+                loot[index].drops[i].quantity = newQuantity;
+                duplicate = true;
+                break;
+            }
+        }
+
+        if (!duplicate) {
+            loot[index].drops.push(item);
+        }
+
         loot[index].kc += 1;
     } else {
         var obj = {
@@ -64,12 +173,73 @@ function addToLootBag(monster, item) {
     }
 }
 
+function paintLootBag(loot) {
+    // #loot-bag
+
+    var lootBag = document.getElementById('loot-bag');
+    lootBag.innerHTML = '';
+
+    for (var i = loot.length - 1; i >= 0; i--) {
+        var lootMonster = document.createElement('div');
+        lootMonster.setAttribute('class', 'row monsterLootBag');
+        var lootMonsterName = document.createElement('h3');
+        lootMonsterName.setAttribute('class', 'col-12');
+
+        var monsterNameString = loot[i].monster;
+        monsterNameString = monsterNameString.replace('%20', ' ');
+
+        lootMonsterName.innerText = monsterNameString;
+
+        lootMonsterKC = document.createElement('span');
+        lootMonsterKC.innerText = ' x'+loot[i].kc;
+
+        lootMonsterName.appendChild(lootMonsterKC);
+
+        lootMonster.appendChild(lootMonsterName);
+        lootBag.appendChild(lootMonster);
+
+        for (var j = 0; j < loot[i].drops.length; j++) {
+            var lootItem = document.createElement('div');
+            lootItem.setAttribute('class', 'col-4');
+
+            lootItemQuantity = loot[i].drops[j].quantity;
+
+            $.ajax({
+                method: "GET",
+                async: false,
+                url: 'https://api.osrsbox.com/items?where={"id": "'+loot[i].drops[j].id+'"}'
+            }).done(function(res) {
+                var lootItemHolder = document.createElement('div');
+                lootItemHolder.setAttribute('class','lootItemHolder');
+                var lootItemImage = document.createElement('img');
+                lootItemImage.setAttribute('src', 'https://www.osrsbox.com/osrsbox-db/items-icons/'+res._items[0].id+'.png');
+                
+                var lootItemText = document.createElement('p');
+                lootItemText.innerText = lootItemQuantity + ' ' + res._items[0].name;
+
+                lootItemHolder.appendChild(lootItemImage);
+                lootItemHolder.appendChild(lootItemText);
+
+                lootItem.appendChild(lootItemHolder);
+                lootMonster.appendChild(lootItem);
+            });
+            // lootItem.innerText = loot[i].drops[j].quantity + ' ' + loot[i].drops[j].name;
+            // lootMonster.appendChild(lootItem);
+        }
+    }
+}
+
+function getItemIcon(id) {
+    $.ajax({
+        method: "GET",
+        url: 'https://api.osrsbox.com/items?where={"id": "'+id+'"}'
+    }).done(function(res) {
+        handleItemDrops(res._items[0].drops, monster);
+    });
+}
+
 $('body').on('click', '.monster', function() {
     var monster = $(this).attr('data-monster');
-    var dropTable = eval(monster+'_items');
-    var item = roll(dropTable);
-    $('#loot-bag').text(item);
-    addToLootBag(monster, item);
-    console.log(loot);
+    getDropTable(monster);
 });
 
--- templates/home.php
+++ templates/home.php
@@ -6,15 +6,15 @@
         <!-- <script src="public/js/hiscores.js"></script> -->
         
         <!-- Base -->
-        <script src="public/js/tables/herbs.js"></script>
+        <!-- <script src="public/js/tables/herbs.js"></script>
         <script src="public/js/tables/megaRare.js"></script>
         <script src="public/js/tables/gems.js"></script>
-        <script src="public/js/tables/rare.js"></script>
+        <script src="public/js/tables/rare.js"></script> -->
 
         <!-- Monsters -->
-        <script src="public/js/tables/monsters/fireGiant.js"></script>
+        <!-- <script src="public/js/tables/monsters/fireGiant.js"></script>
         <script src="public/js/tables/monsters/goblin.js"></script>
-        <script src="public/js/tables/monsters/imp.js"></script>
+        <script src="public/js/tables/monsters/imp.js"></script> -->
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -25,20 +25,338 @@
 
     <body>
 
+        <?php 
+
+            $monsters = [
+                'Fire giant' => [
+                    'image' => 'cave'
+                ],
+                'Goblin' => [
+                    'image' => 'grass-land'
+                ],
+                'Imp' => [
+                    'image' => 'grass-land'
+                ],
+                'Dark wizard' => [
+                    'image' => 'grass-land'
+                ],
+                'Dwarf' => [
+                    'image' => 'mining'
+                ],
+                'Ice giant' => [
+                    'image' => 'ice'
+                ],
+                'Lesser demon' => [
+                    'image' => 'lava'
+                ],
+                'Zulrah' => [
+                    'image' => 'swamp'
+                ],
+                'Vorkath' => [
+                    'image' => 'ice'
+                ],
+                'Flesh Crawler' => [
+                    'image' => 'cave'
+                ],
+                'Aberrant spectre' => [
+                    'image' => 'cave'
+                ],
+                'Ankou' => [
+                    'image' => 'cave'
+                ],
+                'Aviansie' => [
+                    'image' => 'ice'
+                ],
+                'Kree\'arra' => [
+                    'image' => 'ice'
+                ],
+                'King Black Dragon' => [
+                    'image' => 'cave'
+                ],
+                'Dagannoth Rex' => [
+                    'image' => 'water'
+                ],
+                'Chaos Elemental' => [
+                    'image' => 'wildy'
+                ],
+                'Cerberus' => [
+                    'image' => 'cave'
+                ],
+                'Kalphite Queen' => [
+                    'image' => 'sand-cave'
+                ],
+                'Venenatis' => [
+                    'image' => 'wildy'
+                ],
+                'Callisto' => [
+                    'image' => 'wildy'
+                ],
+                'General Graardor' => [
+                    'image' => 'ice'
+                ],
+                'K\'ril Tsutsaroth' => [
+                    'image' => 'ice'
+                ],
+                'Cave horror' => [
+                    'image' => 'cave'
+                ],
+                'Kraken' => [
+                    'image' => 'water'
+                ],
+                'Mithril dragon' => [
+                    'image' => 'cave'
+                ],
+                'Mogre' => [
+                    'image' => 'water'
+                ],
+                'Locust rider' => [
+                    'image' => 'sand-cave'
+                ],
+                'Skeletal Wyvern' => [
+                    'image' => 'ice'
+                ],
+                'Mountain troll' => [
+                    'image' => 'rock'
+                ],
+                'Corporeal Beast' => [
+                    'image' => 'cave'
+                ],
+                'The Nightmare' => [
+                    'image' => 'nightmare'
+                ],
+                'Commander Zilyana' => [
+                    'image' => 'ice'
+                ],
+                'Zalcano' => [
+                    'image' => 'volcano'
+                ],
+                'Colossal Hydra' => [
+                    'image' => 'water'
+                ],
+                'Thermonuclear smoke devil' => [
+                    'image' => 'cave'
+                ],
+                'Hespori' => [
+                    'image' => 'grass-cave'
+                ],
+                'Demonic gorilla' => [
+                    'image' => 'blue-cave'
+                ],
+                'Crazy archaeologist' => [
+                    'image' => 'wildy'
+                ],
+                'The Mimic' => [
+                    'image' => 'mimic'
+                ],
+                'Abyssal Sire' => [
+                    'image' => 'nexus'
+                ],
+                'Scorpia' => [
+                    'image' => 'wildy'
+                ],
+                'Vet\'ion' => [
+                    'image' => 'wildy'
+                ],
+                'Giant Mole' => [
+                    'image' => 'cave'
+                ],
+                'Deranged archaeologist' => [
+                    'image' => 'tar-swamp'
+                ],
+                'Farmer' => [
+                    'image' => 'grass-land'
+                ],
+                'Barbarian' => [
+                    'image' => 'dirt-land'
+                ],
+                'Wizard' => [
+                    'image' => 'grass-water-land'
+                ],
+                'Al-Kharid warrior' => [
+                    'image' => 'palace'
+                ],
+                'Hill Giant' => [
+                    'image' => 'cave'
+                ],
+                // 'Hobgoblin' => [
+                //     'image' => 'grass-water-land'
+                // ],
+                'Black Knight' => [
+                    'image' => 'grass-land'
+                ],
+                'White Knight' => [
+                    'image' => 'grass-land'
+                ],
+                'Moss giant' => [
+                    'image' => 'cave'
+                ],
+                'Ice warrior' => [
+                    'image' => 'ice'
+                ],
+                'Greater demon' => [
+                    'image' => 'lava'
+                ],
+                'H.A.M. Guard' => [
+                    'image' => 'dirt-land'
+                ],
+                'Minotaur' => [
+                    'image' => 'rock'
+                ],
+                'Giant frog' => [
+                    'image' => 'swamp'
+                ],
+                'Rock Crab' => [
+                    'image' => 'rock'
+                ],
+                'Rock Golem' => [
+                    'image' => 'mining'
+                ],
+                // 'Tree spirit' => [
+                //     'image' => 'grass-water-land'
+                // ]
+                'Wall beast' => [
+                    'image' => 'rock'
+                ],
+                'Druid' => [
+                    'image' => 'grass-land'
+                ],
+                'Cockatrice' => [
+                    'image' => 'cave'
+                ],
+                'Sourhog' => [
+                    'image' => 'cave'
+                ],
+                'Banshee' => [
+                    'image' => 'tower'
+                ],
+                'Basilisk' => [
+                    'image' => 'cave'
+                ],
+                'Black demon' => [
+                    'image' => 'cave'
+                ],
+                'Bloodveld' => [
+                    'image' => 'cave'
+                ],
+                'Blue dragon' => [
+                    'image' => 'cave'
+                ],
+                'Dagannoth Supreme' => [
+                    'image' => 'water'
+                ],
+                'Dagannoth Prime' => [
+                    'image' => 'water'
+                ],
+                'Brine rat' => [
+                    'image' => 'cave'
+                ],
+                'Bronze dragon' => [
+                    'image' => 'cave'
+                ],
+                'Chaos druid' => [
+                    'image' => 'cave'
+                ],
+                // 'Dagannoth' => [
+                //     'image' => 'water'
+                // ]
+                'Dark beast' => [
+                    'image' => 'cave'
+                ],
+                'Guardian Drake' => [
+                    'image' => 'rock'
+                ],
+                'Dust devil' => [
+                    'image' => 'cave-smoke'
+                ],
+                'Elf Archer' => [
+                    'image' => 'elf-land'
+                ],
+                'Gargoyle' => [
+                    'image' => 'tower'
+                ],
+                'Revenant imp' => [
+                    'image' => 'wildy'
+                ],
+                'Revenant goblin' => [
+                    'image' => 'wildy'
+                ],
+                'Revenant pyrefiend' => [
+                    'image' => 'wildy'
+                ],
+                'Revenant hobgoblin' => [
+                    'image' => 'wildy'
+                ],
+                'Revenant cyclops' => [
+                    'image' => 'wildy'
+                ],
+                'Revenant hellhound' => [
+                    'image' => 'vz'
+                ],
+                'Revenant demon' => [
+                    'image' => 'wildy'
+                ],
+                'Revenant ork' => [
+                    'image' => 'wildy'
+                ],
+                'Revenant dragon' => [
+                    'image' => 'wildy'
+                ],
+                'Warped Jelly' => [
+                    'image' => 'blue-purple-cave'
+                ],
+                'Jungle horror' => [
+                    'image' => 'jungle'
+                ],
+                'Kalphite Guardian' => [
+                    'image' => 'sand-cave'
+                ],
+                'Lizardman' => [
+                    'image' => 'lizard'
+                ],
+                'Mammoth' => [
+                    'image' => 'wildy'
+                ],
+                'Ancient Zygomite' => [
+                    'image' => 'mushroom'
+                ],
+                'Ogre' => [
+                    'image' => 'grass-water-land'
+                ],
+                'Pirate' => [
+                    'image' => 'ice'
+                ],
+                'Infernal pyrelord' => [
+                    'image' => 'cave'
+                ],
+                'Brutal red dragon' => [
+                    'image' => 'blue-purple-cave'
+                ],
+                'Suqah' => [
+                    'image' => 'grass-water-land'
+                ]
+            ];
+            
+            ksort($monsters);
+        ?>
+
+
         <div class="fluid-container main-container">
-            <div class="row">
-                <div class="col-10 d-flex">
-                    <div class="monster" data-monster="fireGiant" style="background-image:url(public/img/monsters/fire-giant.png), url(public/img/backgrounds/fire-giant.jpg);">
-
-                    </div>
-                    <div class="monster" data-monster="goblin" style="background-image:url(public/img/monsters/goblin.png), url(public/img/backgrounds/grass-path.jpg);">
-
-                    </div>
-                    <div class="monster" data-monster="imp" style="background-image:url(public/img/monsters/imp.png), url(public/img/backgrounds/grass-path.jpg);">
-
-                    </div>
+            <div class="row flex-column-reverse flex-lg-row">
+                <div class="col-lg-8 col-12 d-flex">
+                    <div class="row monster-holder">
+                    <?php
+                        foreach ($monsters as $monster => $background) { ?>
+                        <div class="d-flex align-items-center justify-content-center monster col-3" data-monster="<?=str_replace(' ', '%20', $monster)?>" style="background-image:url(public/img/monsters/<?=strtolower(str_replace("'", "", str_replace(' ','-',$monster)))?>.png), url(public/img/backgrounds/<?=$background['image']?>.jpg);">
+                            <h2 class="text-center">
+                                <?=$monster?>
+                            </h2>
+                        </div>
+                    <?php
+                        }
+                    ?>
                 </div>
-                <div class="col-2">
+                </div>  
+                <div class="col-lg-4 col-12">
                     <aside>
                         <div id="loot-bag">
                             
